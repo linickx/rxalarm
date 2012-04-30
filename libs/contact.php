a9086b50c4d32ca0550c9bf2d7a3c4acef904b2f
Nick <linickx@gmail.com>
2012-04-30 18:00:07 +0100

    Contact Details
    complaints on a post card pls.

--- libs/contact.php
+++ libs/contact.php
@@ -5,6 +5,7 @@
 
 	**/
 
+	$head = "";
 	$title = "Contact";
 	$nav = array('contact' => 'true');
 
@@ -18,7 +19,15 @@
 	<div class="container-fluid">
 		<div class="row-fluid">
 			<div class="span12">
-				<p> Hello!</p>
+				<p> There are a few ways you can get in touch...</p>
+				<h2>Say hi!</h2>
+				<p>I'm on twitter, gimme a shout out via <a href="https://twitter.com/#!/linickx">@linickx</a> :)</p>
+				<h2>Got Issues?</h2>
+				<p>Bugs and problems are to be tracked on Github <i class="icon-arrow-right"></i> <a href="https://github.com/linickx/rxalarm/issues">https://github.com/linickx/rxalarm/issues</a> </p>
+				<h2>Got Fixes?</h2>
+				<p>If you can patch or improve this, please clone <a href="https://github.com/linickx/rxalarm">the github repo</a> and send me a pull request.</p>
+				<h2>Say hi! <small>>160char</small></h2>
+				<p> I have <a href="http://www.linickx.com/contact">a website with a contact page</a> which usually works.</p>
 			</div>
 		</div>
 	</div>
