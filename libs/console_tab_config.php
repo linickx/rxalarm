5a21b29021690ddd57a1884a54e16c47fe1fc7d6
Nick <linickx@gmail.com>
2012-05-01 10:55:19 +0100

    Console/Config - page formatting
    Structure first :)

--- libs/console_tab_config.php
+++ libs/console_tab_config.php
@@ -7,4 +7,37 @@
 
 ?>
 
-<p>Comming Soon...</p>
+<div class="row-fluid">
+	<div class="span2">
+		<!--Sidebar content-->
+		<ul class="nav nav-pills nav-stacked">
+			<li class="active"><a href="#servers" data-toggle="tab">Servers</a></li>
+			<li><a <a href="#checks" data-toggle="tab">Checks</a></li>
+			<li><a <a href="#calarms" data-toggle="tab">Alarms</a></li>
+			<li><a <a href="#notify" data-toggle="tab">Notifications</a></li>
+		</ul>
+	</div>
+	<div class="span10">
+		<!--Body content-->
+
+		<div class="tab-content">
+				
+				<div class="tab-pane active" id="servers">
+					<p>Comming Soon!</p>
+				</div>
+
+				<div class="tab-pane" id="checks">
+					<p>CHECKS</p>
+				</div>
+
+				<div class="tab-pane" id="calarms">
+					<p>ALARMS</p>
+				</div>
+
+				<div class="tab-pane" id="notify">
+					<p>NOTIFY</p>
+				</div>
+		</div>
+
+	</div>
+</div>
