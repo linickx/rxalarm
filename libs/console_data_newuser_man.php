4e525a1096dbc927529a1a2098a6a66a347e6516
Nick <linickx@gmail.com>
2012-05-01 14:31:48 +0100

    Manual WebHook Entry
    User doesn't need to supply their API secret to test :)

--- libs/console_data_newuser_man.php
+++ libs/console_data_newuser_man.php
@@ -4,4 +4,27 @@
 		Data Response to New User (Manual) Request - Used to recieve WebHook supplied by user.
 
 	**/
+
+	$MANWEBHOOK = $_REQUEST['manwebhook'];
+
+	$rs_wh = preg_replace("/[^a-zA-Z0-9]/", "", $MANWEBHOOK); // clean input.
+	$rs_wh = $db->escape($rs_wh);
+
+	$uid = substr($_COOKIE['rxalarm']['uid'], 0, 256);
+	$safeish_uid = $db->escape($uid); 
+	$dbupdate = $db->query("UPDATE user SET rs_wh = \"$rs_wh\" WHERE id = \"$safeish_uid\"");
+
+	if ($dbupdate) {
+
+		$res = 'ok';
+		$msg = '<div class="alert alert-success"><strong>Nice Work!</strong><br />Your WebHook Token Saved.</div><p><a href="#" class="btn" data-dismiss="modal">Close</a></p>';
+
+	} else {
+
+		$res = 'error';
+		$msg = '<div class="alert alert-error"><button class="close" data-dismiss="alert">×</button><strong>Error!</strong><br />Oops, Something went wrong. What did you do?</div>';
+	}
+
+	$output = array ('response'=>$res,'msg'=> $msg);
+	output_json($output);
 ?>
--- libs/console_modal_newuser_manual.php
+++ libs/console_modal_newuser_manual.php
@@ -4,12 +4,37 @@
 	**/
 	?>
 
-<p>Comming Soon</p>
-<!--
-<form class="well">
+<div id="manMSG">
+
+</div>
+
+<form class="well" id="WebHookForm">
+	<input type="hidden" name="d" value="num" />
 	<label>WebHook</label>
-	<input type="text" class="span3" placeholder="">
+	<input name="manwebhook" type="text" class="span3" placeholder="abc123zyx098">
 	<span class="help-inline">Enter Your WebHook Token.</span>
-	<button type="submit" class="btn">Save</button>
+	<button type="submit" id="savetoken" class="btn">Save</button>
 </form>
--->
+
+<script type="text/javascript" charset="utf-8">
+
+$('#savetoken').click(function () { 
+
+	$.ajax({
+		type:'POST', 
+		url:'<?php echo $www;?>/data.php', 
+		data:$('#WebHookForm').serialize(),
+		dataType: "json", 
+		success: function(data) {
+			if (data.response == 'ok') {
+				$("#man").html(data.msg);
+			} else {
+				$("#manMSG").html(data.msg);
+			}
+	}});
+	
+	return false;
+
+});
+
+</script>
